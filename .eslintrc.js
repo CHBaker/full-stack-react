144799c2fa8d0f57a78a149debd357c2b06f9fbe
CHBaker <charlesprobaker@gmail.com>
2022-01-07 14:08:21 -0700

    DEV data and components
    - show articles and metadata
    - change data structure on entry
    - update scripts

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: 'babel-eslint',
+    extends: ['airbnb', 'plugin:prettier/recommended'],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true
+    }
+};
